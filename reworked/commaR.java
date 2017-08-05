2cbf19dd0dcf88f2d9c88df4a939de9d1a05a120
Mohd113 <mad@unreal.cafe>
2017-08-06 00:19:49 +0300

    Comma Separated Values

--- reworked/commaR.java
+++ reworked/commaR.java
@@ -1,47 +1,14 @@
-package csv2;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.nio.file.*;
+import java.util.stream.Stream;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String fline;
-		String cline;
+    public static void main(String[] args) throws Exception {
 		
-		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Chiefan\\Documents\\csv1.txt"))) {
-			fline = br.readLine();
-			for (int i = 1; i <= Integer.parseInt(fline); i++) {
-				cline = br.readLine();
-				int oc = 1;
-				char[] temp = cline.toCharArray();
-				int ii = 0;
-				for (char a : temp) {
-					if (a == '#' || a == '$' || a == '|' || a == '%') {
-						temp[ii] = ',';
-						oc++;	
-					}
-					
-					ii++;
-				}
-				try (BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\Chiefan\\Documents\\csv2.txt", true))) {
-					bw.write(String.valueOf(temp) + "," + oc);
-					bw.newLine();
-				}
-				catch (IOException e1) { 
-					e1.printStackTrace();
-				}
-				System.out.println(String.valueOf(temp) + "," + oc);
-			}
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	try (Stream<String> stream = Files.lines(Paths.get("csv1.txt"))) {
+	    stream.skip(1)
+   	          .map(x -> x = x.replaceAll("[#$%|]", ",") + ",")
+	          .forEach(x -> System.out.printf("%s%d%n", x, x.length()-x.replaceAll(",", "").length()));
 	}
-
+    }
 }
